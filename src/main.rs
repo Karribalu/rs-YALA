e1c71e08d7d6dccd3215060ec99e7f396518b40f
karribalu <karri.balu123456@gmail.com>
2025-04-17 23:35:25 +0100

    feat: Add Map Builder and rooms generator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 mod map;
+mod player;
+mod map_builder;
+
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
@@ -8,20 +11,29 @@ mod prelude {
 }
 
 use prelude::*;
+use crate::map_builder::MapBuilder;
+use crate::player::Player;
+
 struct State {
     map: Map,
+    player: Player,
 }
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new()
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
         }
     }
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 fn main() -> BError {
--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,3 @@
-use std::process::id;
 use crate::prelude::*;
 
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
@@ -41,6 +40,23 @@ impl Map {
                     }
                 }
             }
+        }
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) &&
+            self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
         }
     }
 }
